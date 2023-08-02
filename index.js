6659ffc52d0140d369f17b9821417d031b15e122
Diego Mandu Barreiro - PrestServ <diego.barreiro.prestserv@petrobras.com.br>
2023-08-02 09:37:10 -0300

    #02 Segundo serviço: Fila e Webhook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./src/routes')
-const axios = require('axios')
+
+
 // Conexão com Banco de Dados
 const conn = require('./src/db/connection.js')
 
@@ -9,7 +10,6 @@ const PORT = 3000
 
 app.use(express.json())
 app.use(routes)
-app.use(axios)
 conn()
 
 app.listen(PORT, () => {
--- src/db/connection.js
+++ src/db/connection.js
@@ -3,13 +3,7 @@ require('dotenv').config();
 
 async function main() {
     try {
-        const dbConnectionString = process.env.DB_CONNECTION_STRING;
-        if (!dbConnectionString) {
-            console.error('A string de conexão do banco de dados não foi configurada.');
-            return;
-        }
-
-        await mongoose.connect(dbConnectionString);
+        await mongoose.connect("mongodb+srv://mandudiego:QKE0ypWiyoGfsRho@dblogs.mcixwfl.mongodb.net/logs?retryWrites=true&w=majority")
         console.log('BD connected successful');
     } catch (error) {
         console.log(`Erro: ${error}`);
